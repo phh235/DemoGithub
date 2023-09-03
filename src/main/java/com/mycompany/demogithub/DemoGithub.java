0bab5f5cc45f59ac7e09fd30d1dd3e526ae53d59
phh235 <hoangaatrox23504@gmail.com>
2023-09-03 21:04:11 +0700

    init project

---
+++ src/main/java/com/mycompany/demogithub/DemoGithub.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.demogithub;
+
+/**
+ *
+ * @author HOANG
+ */
+public class DemoGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
